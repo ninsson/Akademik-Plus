cdca675662835f2126b353d366b4466ecb32716f
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-24 20:50:20 +0100

    Defined StatusPokoju and StandardPokoju types

--- backend/internal/models/pokoje.go
+++ backend/internal/models/pokoje.go
@@ -1,1 +1,15 @@
 package models
+
+type StatusPokoju string
+
+const (
+	Dostepny  StatusPokoju = "Dostepny"
+	Wremoncie StatusPokoju = "W_remoncie"
+)
+
+type StandardPokoju string
+
+const (
+	Standard    StandardPokoju = "Standard"
+	Podwyzszony StandardPokoju = "Podwyzszony"
+)
