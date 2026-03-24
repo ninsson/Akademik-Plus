c68221c76df2d533bd63097f5a900a4af16f030b
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-24 21:09:00 +0100

    Defined Uzytkownicy struct

--- backend/internal/models/ludzie.go
+++ backend/internal/models/ludzie.go
@@ -6,3 +6,15 @@ const (
 	Administrator Rola = "Administrator"
 	Mieszkaniec   Rola = "Mieszkaniec"
 )
+
+type Uzytkownicy struct {
+	ID                  int    `db:"id" json:"id"`
+	Imie                string `db:"imie" json:"imie"`
+	Nazwisko            string `db:"nazwisko" json:"nazwisko"`
+	Email               string `db:"email" json:"email"`
+	NumerTelefonu       string `db:"numer_telefonu" json:"numer_telefonu"`
+	Username            string `db:"username" json:"username"`
+	PasswordHash        string `db:"password_hash" json:"password_hash"`
+	Rola                Rola   `db:"rola" json:"rola"`
+	CzyWymagaDostosowan bool   `db:"czy_wymaga_dostosowan" json:"czy_wymaga_dostosowan"`
+}
