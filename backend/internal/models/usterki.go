09193d340488e4e4f3f2a67ffd0ceb11a2ff0a3c
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-24 21:17:54 +0100

    Corrected const name

--- backend/internal/models/usterki.go
+++ backend/internal/models/usterki.go
@@ -12,9 +12,9 @@ const (
 type StatusNaprawy string
 
 const (
-	Przyjeto               StatusNaprawy = "Przyjeto"
-	Weryfikacja            StatusNaprawy = "Weryfikacja"
-	WTrakcieNaprawy        StatusNaprawy = "W_trakcie_naprawy"
-	Naprawiono             StatusNaprawy = "Naprawiono"
-	Zakonczono_bez_naprawy StatusNaprawy = "Zakonczono_bez_naprawy"
+	Przyjeto             StatusNaprawy = "Przyjeto"
+	Weryfikacja          StatusNaprawy = "Weryfikacja"
+	WTrakcieNaprawy      StatusNaprawy = "W_trakcie_naprawy"
+	Naprawiono           StatusNaprawy = "Naprawiono"
+	ZakonczonoBezNaprawy StatusNaprawy = "Zakonczono_bez_naprawy"
 )
