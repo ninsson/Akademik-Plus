a1551ee1737c8b18d731c8af76bb7d44162400d5
Nina Zugaj <ninazugaj25@gmail.com>
2026-04-25 14:41:31 +0200

    Add 2FA page

---
+++ frontend/src/components/AuthCode.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Login.css';
+
+const AuthCode = () => {
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1 className="title">Akademik+</h1>
+        <p className="subtitle">Zaloguj się do systemu</p>
+
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
+          <div className="input-group">
+            <label htmlFor="authCode">Podaj kod autoryzacyjny</label>
+            <input type="text" id="authCode" placeholder="Wpisz kod" />
+          </div>
+
+          <div className="button-center-wrapper">
+            <button type="submit" className="confirm-btn">
+              Zatwierdź
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthCode;
