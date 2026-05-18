c9af809dfdebb412ecd34b9bc987a0202e9adb5f
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-05-18 18:33:22 +0200

    Added PokojID basic validation

--- backend/internal/services/usterki_service.go
+++ backend/internal/services/usterki_service.go
@@ -19,6 +19,9 @@ func (s *UsterkiService) CreateUsterka(u *models.Usterka) error {
 	if u.Priorytet == nil {
 		return errors.New("priorytet zgłoszenia jest wymagany")
 	}
+	if u.PokojID <= 0 {
+		return errors.New("nieprawidłowy identyfikator pokoju")
+	}
 
 	return s.repo.Create(u)
 }
